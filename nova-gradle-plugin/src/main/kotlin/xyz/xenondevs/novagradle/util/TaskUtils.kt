dd0b1a0bbb0a22e7721a5cf0768d5d321c9d121a
Mandlemankiller <mandlemankiller@email.cz>
2025-03-01 22:18:25 +0100

    Fix missing compile artifact error

--- nova-gradle-plugin/src/main/kotlin/xyz/xenondevs/novagradle/util/TaskUtils.kt
+++ nova-gradle-plugin/src/main/kotlin/xyz/xenondevs/novagradle/util/TaskUtils.kt
@@ -10,7 +10,7 @@ import java.nio.file.FileSystems
 import java.nio.file.Path
 
 object TaskUtils {
-    
+
     fun getOutputFile(jar: Jar): File {
         val dir = jar.destinationDirectory.get().asFile
         var name = listOf(
@@ -20,41 +20,41 @@ object TaskUtils {
             jar.archiveClassifier.orNull ?: ""
         ).filterNot(String::isBlank).joinToString("-")
         jar.archiveExtension.orNull?.let { name += ".$it" }
-        
+
         return File(dir, name)
     }
-    
+
     fun findCompileTimeArtifact(project: Project, group: String, name: String): ResolvedArtifactResult {
         for (artifact in project.configurations.getByName("compileClasspath").incoming.artifacts.artifacts) {
             val id = (artifact.id.componentIdentifier as? ModuleComponentIdentifier)
                 ?: continue
-            
+
             if (id.group == group && id.module == name)
                 return artifact
         }
-        
+
         throw IllegalStateException("$group:$name not found in compileClasspath artifacts")
     }
-    
+
     fun findNovaArtifact(project: Project): ResolvedArtifactResult =
-        findCompileTimeArtifact(project, "xyz.xenondevs.nova", "nova")
+        findCompileTimeArtifact(project, "cz.jeme.stevenova", "nova")
     
     fun readNovaAndApiVersion(project: Project): Pair<String, String> {
         val artifact = findNovaArtifact(project)
         val id = artifact.id.componentIdentifier as ModuleComponentIdentifier
         return id.version to readApiVersion(artifact.file.toPath())
     }
-    
+
     private fun readApiVersion(jarPath: Path): String {
         FileSystems.newFileSystem(jarPath).use { fs ->
             val pluginYml = YamlConfigurationLoader.builder()
                 .path(fs.getPath("paper-plugin.yml"))
                 .build()
                 .load()
-            
+
             return pluginYml.node("api-version").string
                 ?: throw IllegalArgumentException("$jarPath contains no paper-plugin.yml with api-version")
         }
     }
-    
+
 }
